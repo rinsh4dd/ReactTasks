f0a2f1733b2f1e826229d764d20b24c44ebff310
rinshad7518 <rinshadcontacts@gmail.com>
2025-06-26 09:56:57 +0530

    machine Task

--- src/components/MachineTask.jsx
+++ src/components/MachineTask.jsx
@@ -6,8 +6,20 @@ function Task() {
   const handleChanges = (e) => {
     setText(e.target.value);
   };
-  const Perform = () => {  
-    text === "Rinshad" || text == "rinshad" ? (setScreen(`My Name is ${text}`)):setScreen("Name is Wrong")
+  const Perform = () => {
+    // text === "Rinshad" || text == "rinshad"
+    //   ? setScreen(`My Name is ${text}`)
+    //   : setScreen("Name is Wrong");\
+
+    if ((text.toLowerCase()==="rinshad")) {
+      setScreen(`My Name is ${text.toUpperCase()}`);
+      setText("")
+    }else if(text.trim()==""){
+      alert("enter a value")
+    }else {
+      setScreen("Name is wrong");
+      setText("")
+    }
   };
   return (
     <>
@@ -17,35 +29,55 @@ function Task() {
           alignItems: "center",
           justifyContent: "center",
           flexDirection: "column",
-          margin:"150px"
+          margin: "150px",
         }}
       >
-        <input style={{borderRadius:"30px",height:"30px",width:"300px", border:"3px solid black",padding:"10px",fontSize:"15px"}}
+        <input
+          style={{
+            borderRadius: "30px",
+            height: "30px",
+            width: "300px",
+            border: "3px solid black",
+            padding: "10px",
+            fontSize: "15px",
+          }}
           type="text"
           placeholder="Enter Your name"
+          value={text}
           onChange={handleChanges}
         />
-        <button style={{margin:'30px',height:"40px",width:"100px",border:"3px solid Black",borderRadius:"20px" , fontWeight:"800px"}} onClick={Perform}>CLICK</button>
+        <button
+          style={{
+            margin: "30px",
+            height: "40px",
+            width: "100px",
+            border: "3px solid Black",
+            borderRadius: "20px",
+            fontWeight: "800px",
+          }}
+          onClick={Perform}
+        >
+          CLICK
+        </button>
 
         <div
           style={{
             width: "350px",
             height: "50px",
             margin: "20px",
-            display:"flex",
-            alignItems:"center",
-            justifyContent:"center",
-            borderRadius:"35px",
-            color:"black",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: "35px",
+            color: "black",
 
-            fontSize:"18px",
-            border:"5px solid black"
+            fontSize: "18px",
+            border: "5px solid black",
           }}
         >
-          
           {screen}
         </div>
-          <p>//enter rinshad to get out</p>
+        <p>//enter rinshad to get out</p>
       </div>
     </>
   );
