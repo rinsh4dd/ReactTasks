b133a59462ba7467973e606011f07c71e3f80054
rinshad7518 <rinshadcontacts@gmail.com>
2025-07-01 16:08:56 +0530

    tasks

--- src/App.jsx
+++ src/App.jsx
@@ -3,46 +3,61 @@ import Apifetch from "./components/Fetch";
 import Task from "./components/MachineTask";
 import RegistrationPage from "./components/Router/RegistrationPage";
 import LoginPage from "./components/Router/LoginPage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/Router/HomePage";
-import CreateBlog from "./components/blogspotApp/CreateBlog";
 import OddOrEven from "./components/OddOrEven/task2";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "./components/blogspotApp/NotFound";
+import BlogsList from "./components/blogspotApp/BlogsList";
+import CreateBlog from "./components/blogspotApp/CreateBlog";
+import BlogDetails from "./components/blogspotApp/BlogDetails";
 function App() {
-  return(
-
-
-      {/* task one */},
-      {/* <ChangeTheme></ChangeTheme> */},
-      {/* <Apifetch></Apifetch> */},
-      {/* <Task></Task> */},
-
-
-
-      {/* Router Task */},
-      {/* <Router>
+  const [blogs, setBlogs] = useState([]);
+  return (
+    {
+      /* task one */
+    },
+    {
+      /* <ChangeTheme></ChangeTheme> */
+    },
+    {
+      /* <Apifetch></Apifetch> */
+    },
+    {
+      /* <Task></Task> */
+    },
+    {
+      /* Router Task */
+    },
+    {
+      /* <Router>
         <Routes>
           <Route path="/homepage" element={<HomePage/>}></Route>
           <Route path="/" element={<RegistrationPage/>}/>
           <Route path="/loginpage" element={<LoginPage/>}/>
         </Routes>
-      </Router> */},
-
+      </Router> */
+    },
+    (
       // odd or even task
 
       // <OddOrEven/>
 
-
-    // {/* blogspotApp */}
-<Router>
-  <Routes>
-    <Route>
-      <Route path="/" element={<CreateBlog/>}/>
-      <Route path="*" element={<NotFound/>}/>
-    </Route>
-  </Routes>
-</Router>
-
+      // {/* blogspotApp */}
+      <Router>
+        <Routes>
+          <Route>
+            <Route
+              path="/"
+              element={<CreateBlog blogs={blogs} setBlogs={setBlogs} />}
+            />
+            <Route path="/bloglist" element={<BlogsList blogs={blogs} />} />
+            <Route path="/blogdetails/:id" element={<BlogDetails blogs={blogs} />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Router>
+    )
   );
 }
 export default App;
